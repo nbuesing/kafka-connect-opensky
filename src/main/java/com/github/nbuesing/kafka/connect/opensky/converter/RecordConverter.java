41871c91af11e3be14b1d1b9edf74bb63662cdd8
Neil Buesing <neil.buesing@objectpartners.com>
2019-06-24 22:00:56 -0500

    move StateVectorField to RecordField and turn into an enum with schema defined within, simplifying RecordConverter.

--- src/main/java/com/github/nbuesing/kafka/connect/opensky/converter/RecordConverter.java
+++ src/main/java/com/github/nbuesing/kafka/connect/opensky/converter/RecordConverter.java
@@ -5,11 +5,10 @@ import com.github.nbuesing.kafka.connect.opensky.api.Record;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
-import org.apache.kafka.connect.data.Timestamp;
 
 import java.util.Date;
 
-import static com.github.nbuesing.kafka.connect.opensky.converter.StateVectorField.*;
+import static com.github.nbuesing.kafka.connect.opensky.converter.RecordField.*;
 
 public final class RecordConverter {
 
@@ -23,29 +22,29 @@ public final class RecordConverter {
             .version(1)
             .optional()
             .doc("geolocation position")
-            .field("latitude", SchemaBuilder.float64().doc("WGS-84 latitude in decimal degrees.").build())
-            .field("longitude", SchemaBuilder.float64().doc("WGS-84 longitude in decimal degrees.").build())
+            .field(LATITUDE.field(), LATITUDE.schema())
+            .field(LONGITUDE.field(), LONGITUDE.schema())
             .build();
 
     public static final Schema SCHEMA = SchemaBuilder.struct()
             .name("com.github.nbuesing.kafka.kafka.connect.opensky.OpenSky")
             .version(1)
-            .field(IACO24.getField(), SchemaBuilder.string().doc(IACO24.getDoc()).build())
-            .field(CALL_SIGN.getField(), SchemaBuilder.string().optional().doc(CALL_SIGN.getDoc()).build())
-            .field(ORIGIN_COUNTRY.getField(), SchemaBuilder.string().optional().doc(ORIGIN_COUNTRY.getDoc()).build())
-            .field(LAST_POSITION_UPDATE.getField(), Timestamp.builder().optional().doc(LAST_POSITION_UPDATE.getDoc()).build())
-            .field(LAST_CONTACT.getField(), Timestamp.builder().optional().doc(LAST_CONTACT.getDoc()).build())
-            .field(GEO_LOCATION.getField(), GEO_LOCATION_SCHEMA)
-            .field(BARO_ALTITUDE.getField(), SchemaBuilder.float64().optional().doc(BARO_ALTITUDE.getDoc()).build())
-            .field(ON_GROUND.getField(), SchemaBuilder.bool().optional().doc(ON_GROUND.getDoc()).build())
-            .field(VELOCITY.getField(), SchemaBuilder.float64().optional().doc(VELOCITY.getDoc()).build())
-            .field(HEADING.getField(), SchemaBuilder.float64().optional().doc(HEADING.getDoc()).build())
-            .field(VERTICAL_RATE.getField(), SchemaBuilder.float64().optional().doc(VERTICAL_RATE.getDoc()).build())
+            .field(IACO24.field(), IACO24.schema())
+            .field(CALL_SIGN.field(), CALL_SIGN.schema())
+            .field(ORIGIN_COUNTRY.field(), ORIGIN_COUNTRY.schema())
+            .field(LAST_POSITION_UPDATE.field(), LAST_POSITION_UPDATE.schema())
+            .field(LAST_CONTACT.field(), LAST_CONTACT.schema())
+            .field("location", GEO_LOCATION_SCHEMA)
+            .field(BARO_ALTITUDE.field(), BARO_ALTITUDE.schema())
+            .field(ON_GROUND.field(), ON_GROUND.schema())
+            .field(VELOCITY.field(), VELOCITY.schema())
+            .field(HEADING.field(), HEADING.schema())
+            .field(VERTICAL_RATE.field(), VERTICAL_RATE.schema())
             //todo sensors int[]
-            .field(GEO_ALTITUDE.getField(), SchemaBuilder.float64().optional().doc(GEO_ALTITUDE.getDoc()).build())
-            .field(SQUAWK.getField(), SchemaBuilder.string().optional().doc(SQUAWK.getDoc()).build())
-            .field(SPI.getField(), SchemaBuilder.bool().optional().doc(SPI.getDoc()).build())
-            .field(POSITION_SOURCE.getField(), SchemaBuilder.string().optional().doc(POSITION_SOURCE.getDoc()).build())
+            .field(GEO_ALTITUDE.field(), GEO_ALTITUDE.schema())
+            .field(SQUAWK.field(), SQUAWK.schema())
+            .field(SPI.field(), SPI.schema())
+            .field(POSITION_SOURCE.field(), POSITION_SOURCE.schema())
             .build();
 
 
@@ -53,26 +52,26 @@ public final class RecordConverter {
 
         final Struct struct = new Struct(SCHEMA);
 
-        struct.put(IACO24.getField(), record.getIcao24());
-        struct.put(CALL_SIGN.getField(), record.getCallSign());
-        struct.put(ORIGIN_COUNTRY.getField(), record.getOriginCountry());
-        struct.put(LAST_POSITION_UPDATE.getField(), fromEpoc(record.getTimePosition()));
-        struct.put(LAST_CONTACT.getField(), fromEpoc(record.getLastContact()));
-        struct.put(ORIGIN_COUNTRY.getField(), record.getOriginCountry());
-        struct.put(GEO_LOCATION.getField(),
+        struct.put(IACO24.field(), record.getIcao24());
+        struct.put(CALL_SIGN.field(), record.getCallSign());
+        struct.put(ORIGIN_COUNTRY.field(), record.getOriginCountry());
+        struct.put(LAST_POSITION_UPDATE.field(), fromEpoc(record.getTimePosition()));
+        struct.put(LAST_CONTACT.field(), fromEpoc(record.getLastContact()));
+        struct.put(ORIGIN_COUNTRY.field(), record.getOriginCountry());
+        struct.put("location",
                 new Struct(GEO_LOCATION_SCHEMA)
-                        .put(LATITUDE.getField(), record.getLatitude())
-                        .put(LONGITUDE.getField(), record.getLongitude())
+                        .put(LATITUDE.field(), record.getLatitude())
+                        .put(LONGITUDE.field(), record.getLongitude())
         );
-        struct.put(BARO_ALTITUDE.getField(), record.getBaroAltitude());
-        struct.put(ON_GROUND.getField(), record.getOnGround());
-        struct.put(VELOCITY.getField(), record.getVelocity());
-        struct.put(HEADING.getField(), record.getTrueTrack());
-        struct.put(VERTICAL_RATE.getField(), record.getVerticalRate());
-        struct.put(GEO_ALTITUDE.getField(), record.getGeoAltitude());
-        struct.put(SQUAWK.getField(), record.getSquawk());
-        struct.put(SPI.getField(), record.getSpi());
-        struct.put(POSITION_SOURCE.getField(), record.getPositionSource() != null ? "" + record.getPositionSource() : null);
+        struct.put(BARO_ALTITUDE.field(), record.getBaroAltitude());
+        struct.put(ON_GROUND.field(), record.getOnGround());
+        struct.put(VELOCITY.field(), record.getVelocity());
+        struct.put(HEADING.field(), record.getTrueTrack());
+        struct.put(VERTICAL_RATE.field(), record.getVerticalRate());
+        struct.put(GEO_ALTITUDE.field(), record.getGeoAltitude());
+        struct.put(SQUAWK.field(), record.getSquawk());
+        struct.put(SPI.field(), record.getSpi());
+        struct.put(POSITION_SOURCE.field(), record.getPositionSource() != null ? "" + record.getPositionSource() : null);
 
         return struct;
     }
---
+++ src/main/java/com/github/nbuesing/kafka/connect/opensky/converter/RecordField.java
@@ -0,0 +1,43 @@
+package com.github.nbuesing.kafka.connect.opensky.converter;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Timestamp;
+
+
+public enum RecordField {
+
+    IACO24("icao24", SchemaBuilder.string().doc("Unique ICAO 24-bit address of the transponder in hex string representation.").build()),
+    CALL_SIGN("callSign", SchemaBuilder.string().optional().doc("Callsign of the vehicle (8 chars). Can be null if no callsign has been received.").build()),
+    ORIGIN_COUNTRY("originCountry", SchemaBuilder.string().optional().doc("Country name inferred from the ICAO 24-bit address.").build()),
+    LAST_POSITION_UPDATE("timePosition", Timestamp.builder().optional().doc("Unix timestamp (seconds) for the last position update. Can be null if no position report was received by OpenSky within the past 15s.").build()),
+    LAST_CONTACT("lastContact", Timestamp.builder().optional().doc("Unix timestamp (seconds) for the last update in general. This field is updated for any new, valid message received from the transponder.").build()),
+    LONGITUDE("longitude", SchemaBuilder.float64().doc("geographical location in WSG-84 decimal degrees.").build()),
+    LATITUDE("latitude", SchemaBuilder.float64().doc("WGS-84 longitude in decimal degrees.").build()),
+    BARO_ALTITUDE("barometricAltitude", SchemaBuilder.float64().optional().doc("Barometric altitude in meters. Can be null.").build()),
+    ON_GROUND("onGround", SchemaBuilder.bool().optional().doc("Boolean value which indicates if the position was retrieved from a surface position report.").build()),
+    VELOCITY("velocity", SchemaBuilder.float64().optional().doc("Velocity over ground in m/s. Can be null.").build()),
+    HEADING("heading", SchemaBuilder.float64().optional().doc("True track in decimal degrees clockwise from north (north=0°). Can be null.").build()),
+    VERTICAL_RATE("verticalRate", SchemaBuilder.float64().optional().doc("Vertical rate in m/s. A positive value indicates that the airplane is climbing, a negative value indicates that it descends. Can be null.").build()),
+    GEO_ALTITUDE("geometricAltitude", SchemaBuilder.float64().optional().doc("Geometric altitude in meters. Can be null.").build()),
+    SQUAWK("squawk", SchemaBuilder.string().optional().doc("The transponder code aka Squawk. Can be null.").build()),
+    SPI("specialPurpose", SchemaBuilder.bool().optional().doc("Whether flight status indicates special purpose indicator.").build()),
+    POSITION_SOURCE("positionSource", SchemaBuilder.string().optional().doc("Origin of this state’s position: ADS-B, ASTERIX, MLAT, FLAM, UNKNOWN").build());
+
+    private String field;
+    private Schema schema;
+
+    RecordField(final String field, final Schema schema) {
+        this.field = field;
+        this.schema = schema;
+    }
+
+    public String field() {
+        return this.field;
+    }
+
+    public Schema schema() {
+        return this.schema;
+    }
+
+}
